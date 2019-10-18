f4af1a98048b8d511d5cfbf8c8de1ef1cd2970ed
theophile <theophile.braye@gmail.com>
2019-10-18 16:23:06 +0200

    Merge en cours

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import View.ViewHandler;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(ViewHandler.class);
+    }
+}
