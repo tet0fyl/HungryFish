424858c0344bbb47b86b56115add0704ebaf18f3
tet0 <theophile.braye@gmail.com>
2019-10-22 22:31:13 +0200

    Ajout effet parallax au MenuPrincipal. Creation Model Parallax

--- src/Model/ModelMenuPrincipal.java
+++ src/Model/ModelMenuPrincipal.java
@@ -1,6 +1,7 @@
 package Model;
 
 
+import Tool.Path;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.scene.Group;
@@ -10,9 +11,11 @@ import java.util.ArrayList;
 public class ModelMenuPrincipal {
 
     public BubblePop bubbles;
+    public Parallax parallax;
 
     public ModelMenuPrincipal(){
         bubbles = new BubblePop();
+        parallax = new Parallax(Path.urlParallaxMenuPrincipalBackgroundImg);
     }
 
 
