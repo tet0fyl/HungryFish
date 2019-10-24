90b730543c7207b6b7a426655533a22f3182b71b
sarah-sb <sarah.sbini@gmail.fr>
2019-10-24 16:52:11 +0200

    Mise en page de la vue Option en cours

--- src/View/ViewMenuOption.java
+++ src/View/ViewMenuOption.java
@@ -13,11 +13,12 @@ import javafx.stage.Screen;
 
 public class ViewMenuOption {
 
-    private Group root;
+    private Group root, grpFish;
     private Text txtTitle;
-    private Text testText;
+    private Text testText, selectSkin;
     private Button btnReturn, btnValider;
     private ImageView imgBackground;
+    private ImageView fish1, fish2, fish3, fish4, fish5;
 
     ViewMenuOption(Group root){
         this.root = root;
@@ -28,22 +29,57 @@ public class ViewMenuOption {
 
         testText = new Text(100,100,"On est dans la vue des options");
 
+
         btnValider = new Button("Valider");
-        btnValider.setLayoutX(200);
-        btnValider.setLayoutY(400);
+        btnValider.getStyleClass().add("btn");
+        btnValider.setLayoutX(750);
+        btnValider.setLayoutY(800);
 
         btnReturn = new Button("Retour");
-        btnReturn.setLayoutX(200);
-        btnReturn.setLayoutY(200);
+        btnReturn.getStyleClass().add("btn");
+        btnReturn.setLayoutX(1000);
+        btnReturn.setLayoutY(800);
 
         initBackground();
+
+        selectSkin = new Text(800, 500, "Select your skin");
+        selectSkin.setFont(Font.loadFont(ViewMenuOption.class.getResourceAsStream(Path.fontWavePool), 30));
+
+        fish1 = initFish();
+        fish1.setX(0);
+        fish1.setY(0);
+        fish2 = initFish();
+        fish2.setX(150);
+        fish2.setY(0);
+        fish3 = initFish();
+        fish3.setX(300);
+        fish3.setY(0);
+        fish4 = initFish();
+        fish4.setX(450);
+        fish4.setY(0);
+        fish5 = initFish();
+        fish5.setX(600);
+        fish5.setY(0);
+
+        grpFish = new Group();
+        grpFish.getChildren().add(fish1);
+        grpFish.getChildren().add(fish2);
+        grpFish.getChildren().add(fish3);
+        grpFish.getChildren().add(fish4);
+        grpFish.getChildren().add(fish5);
+        grpFish.setLayoutX(600);
+        grpFish.setLayoutY(500);
 
         root.getChildren().clear();
         root.getChildren().add(imgBackground);
         root.getChildren().add(txtTitle);
         root.getChildren().add(testText);
+        root.getChildren().add(selectSkin);
         root.getChildren().add(btnValider);
         root.getChildren().add(btnReturn);
+        root.getChildren().add(grpFish);
+
+
     }
 
     public Text initText(int fontSize, String textContent){
@@ -59,6 +95,13 @@ public class ViewMenuOption {
         Rectangle2D psb = Screen.getPrimary().getBounds();
         imgBackground.setFitHeight(psb.getHeight());
         imgBackground.setFitWidth(psb.getWidth());
+    }
+
+    public ImageView initFish(){
+        ImageView fish = new ImageView(Path.firstFish);
+        fish.setFitWidth(100);
+        fish.setFitHeight(100);
+        return fish;
     }
 
     public void setEvents(ControllerMenuOption model){
