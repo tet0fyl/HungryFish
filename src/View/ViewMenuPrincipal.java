f5fa3a540929b7b0c331a7d635b25690ee24f90a
tet0 <theophile.braye@gmail.com>
2019-11-08 00:03:21 +0100

    Correction de la mise en forme du MenuPrincipal

--- src/View/ViewMenuPrincipal.java
+++ src/View/ViewMenuPrincipal.java
@@ -4,7 +4,9 @@ import Controller.ControllerMenuPrincipal;
 import Model.ModelMenuPrincipal;
 import Tool.Path;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -18,18 +20,23 @@ public class ViewMenuPrincipal{
     private ModelMenuPrincipal model;
     private Label txtTitle;
     private Button btnStart, btnOption, btnExit;
+    private VBox vBox; // <== On declare la variable privé pour quelle soit accessible en dehors de la classe constructor (pour le setEvents plus bas)
 
     ViewMenuPrincipal(Group root, ModelMenuPrincipal model){
         this.root = root;
         this.model = model;
 
-        VBox vBox = new VBox();
-        vBox.setPadding(new Insets(10, 0, 50, 100));
-        vBox.setSpacing(70);
+        //VBox vBox = new VBox();
+        vBox = new VBox(); //<== On initialise la vBox
+        vBox.setMinWidth(Screen.getPrimary().getBounds().getWidth()); //<== Je donne une taille Minimun a ma vBox qui est la largueur de l´écran
+        vBox.setAlignment(Pos.CENTER); //<== Je dis a met element de se mettre au centre de la vBox
+        //vBox.setPadding(new Insets(10, 0, 50, 100));
+        vBox.setSpacing(25); //<== J'ai baisser un peu le Padding mais tu peux le monter si tu preferes, y a pas de soucis :)
 
-        txtTitle = initText(90, "HUNGRY FISH");
+        txtTitle = initText(100, "HUNGRY FISH");
         vBox.getChildren().add(txtTitle);
-        vBox.setLayoutX(400);
+        VBox.setMargin(txtTitle,new Insets(100,0,50,0)); // <== Je met une marge Top et Bottom a mon titre pour pas qu'elle depasse l'ecran ou quelle touche un bouton
+        //vBox.setLayoutX(400);
 
         btnStart = initBtn(20, "JOUER");
         vBox.getChildren().add(btnStart);
@@ -68,11 +75,16 @@ public class ViewMenuPrincipal{
         btnStart.setOnMouseClicked(mc);
         btnOption.setOnMouseClicked(mc);
         btnExit.setOnMouseClicked(mc);
-        model.parallax.getRoot().setOnMouseMoved(mc);
-        btnStart.setOnMouseMoved(mc);
-        btnOption.setOnMouseMoved(mc);
-        btnExit.setOnMouseMoved(mc);
-        txtTitle.setOnMouseMoved(mc);
+
+        for(Node node: vBox.getChildren()){  // <== Je met un evenement sur le mouvement de la souris pour pas perdre le focus du parallax
+            node.setOnMouseMoved(mc);
+        }
+
+        //model.parallax.getRoot().setOnMouseMoved(mc);
+        //btnStart.setOnMouseMoved(mc);
+        //btnOption.setOnMouseMoved(mc);
+        //btnExit.setOnMouseMoved(mc);
+        //txtTitle.setOnMouseMoved(mc);
     }
 
     public Button getBtnStart(){return btnStart;}
